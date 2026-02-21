0b61240d774fea20b5c60610d6543414714cd7b3
imanchalsingh <singhanchal159357@gmail.com>
2026-02-21 17:05:21 +0530

    Update Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Education from "./components/Education";
@@ -17,8 +15,6 @@ function App() {
       <About />
       <Projects />
       <Education />
-      <Experience />
-      <Certificates />
       <Contact />
       <Footer />
     </div>
